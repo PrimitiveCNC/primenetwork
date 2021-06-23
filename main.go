e0ff760ac5beac5064535b2d1fe75c9acb3e9603
PrimitiveCNC <86333892+PrimitiveCNC@users.noreply.github.com>
2021-06-22 20:59:23 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var (
+	i int
+	timeout int = 10
+)
+
+func spam(url string) {
+	for {
+		http.Get(url)
+	}
+}
+func spamget(url string) {
+	go spam(url)
+	fmt.Println("Get spamming started in the Backround")
+}
+
+func get(url string) {
+	go ghettocount(timeout)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+   body, err := ioutil.ReadAll(resp.Body)
+   if err != nil {
+      log.Fatalln(err)
+   }
+   sb := string(body)
+   log.Printf(sb)
+   fmt.Println(sb)
+}
+
+func getnoreply(url string) {
+	http.Get(url)
+}
+
+func getnoreplybackground(url string) {
+	go getnoreply(url)
+}
+
+func ghettocount(num int) {
+	i = 0
+	for {
+		count := i + 1
+		i = count
+		if i == num {
+			goto done;
+		}
+	}
+	done:
+	fmt.Println("Timeout Exceeded Max Int")
+
+}
+
+func getfast(url string) {
+	go get(url)
+	fmt.Println("Request Sending In The Backround.")
+}
